d2f23285b5aa9035c028e565770b611177b65720
sanishchirayath1 <sanishchirayath@gmail.com>
2022-01-08 12:51:10 +0530

    navbar styling done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,22 @@
 import navbarStyles from '../styles/Navbar.module.scss';
+import requests from '../utils/requests';
+import { useRouter } from 'next/router';
 
 function Navbar() {
+    const router = useRouter();
     return (
         <nav className={navbarStyles.container}>
-            this is the navbar
+            <div>
+                {Object.entries(requests).map(([key, {title, url}]) => (
+                    <h2 
+                    key={key}
+                    onClick={() => router.push(url)}
+                    >
+                        {title}
+                    </h2>
+                ))}
+            </div>
+            <div className={navbarStyles.fade}></div>
         </nav>
     )
 }
